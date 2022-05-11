14ba5e5147dd09582a2683d2e750a3905c4f1b61
memoTux <romeo@mendezfuentes.net>
2022-05-11 16:23:07 -0600

    Quasar Module

--- README.md
+++ README.md
@@ -1,6 +1,53 @@
-# Nuxt Module
+# Quasar for Nuxt
+
+This Nuxt module expose UI Components and Utils (Composables, Directives and Plugins) from Quasar Framework to Nuxt.
+This is done by using `quasar-vite-plugin`. For this reason this module only can be use on Nuxt with Vite: `nuxt ^3.0.0-rc.2`.
+
+## Install
+
+At your Nuxt project folder:
+
+```
+sh
+
+# Install dependencies
+yarn add quasar nuxt-quasar @quasar/extras
+```
+
+`@quasar/extra` is optional
+
+```
+ts
+nuxt.config.ts
+
+defineNuxtConfig({
+  // Add nuxt-quasar to modules
+  modules: ['nuxt-quasar'],
+
+  // Optionaly use 'quasar' configKey
+  // This are the defaults
+  quasar: {
+    sassVariables: 'assets/quasar.variables.scss', // Optional string | boolean
+    css: ['quasar/src/css/index.sass'] // Optional string[]
+    vue: {
+      plugins: ['Notify'], // List of extra Quasar Plugins
+      // This Object accepts all options of framework.config on quasar.config.ts
+      config: {
+        dark: true
+      }
+    } 
+  }
+})
+```
+
+For more information, you can read the Quasar Vite Plugin docs, and `quasar.config.ts` framework docs.
+
+## TODO
+
+- Directives like `v-ripple` doesn't work.
+- Own 'quasar-install'
 
 ## Development
 
-- Run `npm run dev:prepare` to generate type stubs.
-- Use `npm run dev` to start [playground](./playground) in development mode.
+- Run `yarn dev:prepare` to generate type stubs.
+- Use `yarn dev` to start [playground](./playground) in development mode.
--- src/module.ts
+++ src/module.ts
@@ -1,24 +1,128 @@
-import { resolve } from 'path'
-import { fileURLToPath } from 'url'
-import { defineNuxtModule, addPlugin } from '@nuxt/kit'
+import type { Plugin } from 'vite'
+import {
+  defineNuxtModule,
+  addPluginTemplate
+} from '@nuxt/kit'
+import { quasar } from '@quasar/vite-plugin'
+import { genObjectFromRawEntries } from 'knitwork'
 
-export interface ModuleOptions {
-  addPlugin: boolean
+interface ModuleOptions {
+  sassVariables?: string | boolean
+  css?: string[]
+  plugins?: ['Notify' | 'Dialog']
+  config?: {
+    dark: boolean
+  }
 }
 
 export default defineNuxtModule<ModuleOptions>({
   meta: {
-    name: 'my-module',
-    configKey: 'myModule'
-  },
-  defaults: {
-    addPlugin: true
-  },
-  setup (options, nuxt) {
-    if (options.addPlugin) {
-      const runtimeDir = fileURLToPath(new URL('./runtime', import.meta.url))
-      nuxt.options.build.transpile.push(runtimeDir)
-      addPlugin(resolve(runtimeDir, 'plugin'))
+    // Usually  npm package name of your module
+    name: 'quasar-nuxt',
+    // The key in `nuxt.config` that holds your module options
+    configKey: 'quasar',
+    // Compatibility constraints
+    compatibility: {
+      // Semver version of supported nuxt versions
+      nuxt: '^3.0.0'
     }
+  },
+  // Default configuration options for your module
+  defaults: {
+    sassVariables: true,
+    css: ['quasar/src/css/index.sass'],
+    plugins: ['Notify'],
+    config: {
+      dark: true
+    }
+  },
+  hooks: {
+    'autoImports:sources': (presets) => {
+      presets.push({
+        from: 'quasar/src/composables',
+        imports: [
+          'useQuasar',
+          'useDialogPluginComponent',
+          'useFormChild'
+        ]
+      }, {
+        from: 'quasar/src/utils',
+        imports: [
+          // ['clone', 'qClone'],
+          // ['colors', 'qColors'],
+          // ['copy-to-clipboard', 'qCopyToClipboard'],
+          ['date', 'qDate'],
+          // ['debounce', 'qDebounce'],
+          // ['dom', 'qDom'],
+          // ['event', 'qEvent'],
+          // ['export-file', 'qExportFile'],
+          // ['extend', 'qExtend'],
+          // ['format', 'qFormat'],
+          // ['morph', 'qMorph'],
+          // ['open-url', 'qOpenUrl'],
+          // ['patterns', 'qPatterns'],
+          // ['prevent-scroll', 'qPreventScroll'],
+          // ['scroll', 'qScroll'],
+          // ['throttle', 'qThrottle'],
+          ['uid', 'qUid']
+        ]
+      })
+    },
+    'components:dirs': (dirs) => {
+      dirs.push({
+        path: '@/node_modules/quasar/src/components',
+        transpile: true,
+        watch: false,
+        pattern: '**/Q**.js',
+        extendComponent: (component) => {
+          return {
+            ...component,
+            export: 'default',
+            pascalName: component.pascalName.slice(
+              component.pascalName.indexOf('Q')
+            ),
+            kebabName: component.kebabName.slice(
+              component.kebabName.indexOf('q')
+            )
+          }
+        }
+      })
+    }
+  },
+  setup: (opts, nuxt) => {
+    nuxt.options.css.push(...opts.css)
+
+    nuxt.hook('vite:extendConfig', (config, { isServer }) => {
+      const vitePlugins = quasar({
+        runMode: isServer ? 'ssr-server' : 'web-client',
+        sassVariables: opts.sassVariables
+      }) as unknown as Plugin[]
+
+      config.plugins.push(vitePlugins)
+
+      return config
+    })
+
+    addPluginTemplate({
+      filename: 'plugins/quasarVueUse.ts',
+      mode: 'client',
+      write: true,
+      getContents: () => {
+        const config = genObjectFromRawEntries(Object.entries(opts.config), '\t\t')
+        const plugins = opts.plugins.join(',')
+
+        return `
+import Quasar from "quasar/src/vue-plugin"
+import { ${plugins} } from "quasar/src/plugins"
+
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.use(Quasar, {
+    plugins: { ${plugins} },
+    config: ${config},
+  })
+})
+`
+      }
+    })
   }
 })
