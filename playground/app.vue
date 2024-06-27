2463f58d8463647692f02d17c882c7be50317cba
memoTux <romeo@mendezfuentes.net>
2024-06-27 09:20:40 -0600

    [playground] add layout default

--- playground/app.vue
+++ playground/app.vue
@@ -1,6 +1,12 @@
 <template>
-  <LayoutMain>
-    <QBtn label="Quasar + Nuxt3" icon="bookmark" size="xl" color="primary" @click="playNotify" />
+  <NuxtLayout>
+    <QBtn
+      label="Quasar + Nuxt3"
+      icon="bookmark"
+      size="xl"
+      color="primary"
+      @click="playNotify"
+    />
     <div
       v-ripple
       class="relative-position container column flex-center text-white"
@@ -10,49 +16,49 @@
       <p>{{ $q.screen.name }}</p>
       <p>{{ newDate }}</p>
     </div>
-  </LayoutMain>
+  </NuxtLayout>
 </template>
 
 <script setup>
-const $q = useQuasar()
+const $q = useQuasar();
 const playNotify = () => {
   // eslint-disable-next-line no-console
-  console.log('Playing Notify')
-  $q.notify('Congrats! you have: Quasar + Nuxt3')
-}
+  console.log("Playing Notify");
+  $q.notify("Congrats! you have: Quasar + Nuxt3");
+};
 // `v-ripple` issue is fixed on `quasar v2.7.3`
 // https://github.com/quasarframework/quasar/issues/13732#issuecomment-1159682150
 // defineExpose({ $q })
 const colors = [
-  'primary',
-  'amber',
-  'secondary',
-  'orange',
-  'accent',
-  'lime',
-  'cyan',
-  'purple',
-  'brown',
-  'blue'
-]
-const color = ref(colors[0])
-const index = ref(0)
+  "primary",
+  "amber",
+  "secondary",
+  "orange",
+  "accent",
+  "lime",
+  "cyan",
+  "purple",
+  "brown",
+  "blue",
+];
+const color = ref(colors[0]);
+const index = ref(0);
 
-let timer
+let timer;
 
 onMounted(() => {
   timer = setInterval(() => {
-    index.value = (index.value + 1) % colors.length
-    color.value = colors[index.value]
-  }, 3000)
-})
+    index.value = (index.value + 1) % colors.length;
+    color.value = colors[index.value];
+  }, 3000);
+});
 
 onBeforeUnmount(() => {
-  clearTimeout(timer)
-})
-const classes = computed(() => `bg-${color.value}`)
+  clearTimeout(timer);
+});
+const classes = computed(() => `bg-${color.value}`);
 
-const newDate = qdate.addToDate(new Date(), { days: 7, months: 1 })
+const newDate = qdate.addToDate(new Date(), { days: 7, months: 1 });
 </script>
 
 <style lang="sass">
