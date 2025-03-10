fbffaa1a4a93e21131ef804e706a038ad016814f
memoTux <romeo@mendezfuentes.net>
2025-03-10 14:48:11 -0600

    chore(playground): cleanup

--- playground/app.vue
+++ playground/app.vue
@@ -26,9 +26,7 @@ const playNotify = () => {
   console.log("Playing Notify");
   $q.notify("Congrats! you have: Quasar + Nuxt3");
 };
-// `v-ripple` issue is fixed on `quasar v2.7.3`
-// https://github.com/quasarframework/quasar/issues/13732#issuecomment-1159682150
-// defineExpose({ $q })
+
 const colors = [
   "primary",
   "amber",
