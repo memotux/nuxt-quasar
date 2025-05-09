ea91f8ba46abb30d0ade743f15af6352320255ad
memoTux <romeo@mendezfuentes.net>
2025-05-09 10:47:55 -0600

    fix: lint errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { createConfigForNuxt } from "@nuxt/eslint-config/flat";
+import { createConfigForNuxt } from '@nuxt/eslint-config/flat'
 
 // Run `npx @eslint/config-inspector` to inspect the resolved config interactively
 export default createConfigForNuxt({
@@ -10,9 +10,9 @@ export default createConfigForNuxt({
     stylistic: true,
   },
   dirs: {
-    src: ["./playground"],
+    src: ['./playground'],
   },
 })
   .append
   // your custom flat config here...
-  ();
+  ()
